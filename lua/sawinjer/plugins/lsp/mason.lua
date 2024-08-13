661223fc064b66848a5c7f07d710dbf90cb4a0d0
sawinjer <github.liubeshiv@gmail.com>
2024-08-13 23:32:49 +0300

    eslint

--- lua/sawinjer/plugins/lsp/mason.lua
+++ lua/sawinjer/plugins/lsp/mason.lua
@@ -1,44 +1,45 @@
 local mason_status, mason = pcall(require, "mason")
 if not mason_status then
-  return
+	return
 end
 
 local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status then
-  return
+	return
 end
 
-function TableConcat(t1,t2)
-    for i=1,#t2 do
-        t1[#t1+1] = t2[i]
-    end
-    return t1
+function TableConcat(t1, t2)
+	for i = 1, #t2 do
+		t1[#t1 + 1] = t2[i]
+	end
+	return t1
 end
 
 lsp_default_servers = {
-    "html",
-    "cssls",
-    "tailwindcss",
-    "clangd",
-    "gopls",
-    "rome", -- json
-    "marksman", -- markdown
-    "psalm", -- php
-    "ruff", -- python
-    "rust_analyzer",
-    "sqlls",
-    "yamlls",
-    "taplo", -- toml
-    "emmet_ls",
-    "volar", -- Vue
+	"html",
+	"cssls",
+	"tailwindcss",
+	"clangd",
+	"gopls",
+	"rome", -- json
+	"marksman", -- markdown
+	"psalm", -- php
+	"ruff", -- python
+	"rust_analyzer",
+	"sqlls",
+	"yamlls",
+	"taplo", -- toml
+	"emmet_ls",
+	"volar", -- Vue
 }
 
 lsp_all_serveres = TableConcat(lsp_default_servers, {
-    "tsserver",
-    "lua_ls",
+	"tsserver",
+	"lua_ls",
+	"eslint",
 })
 
 mason.setup()
 mason_lspconfig.setup({
-  ensure_installed = lsp_all_serveres
+	ensure_installed = lsp_all_serveres,
 })
